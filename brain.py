358eac00cbf96f70abd0490f97e66fb2de07191c
Andres Berejnoi <andresberejnoi@gmail.com>
2021-09-27 16:08:50 -0400

    added basic motion capability using only random values, not real thinking

--- brain.py
+++ brain.py
@@ -1,6 +1,12 @@
+import numpy as np
+
 class Brain(object):
     def __init__(self, params_dict=None):
         if params_dict is None:
             pass 
 
         
+    def make_decision(self, inputs=None):
+        #inputs will be an array of different sensor data for the organism
+        #outputs will be the amount of distance to travel (0 to 1), and the direction (as an angle)
+        return np.random.rand(2)    #will create an array of 2 elements, each with a random value [0-1]
