2d167646277f3064ca10386fe08e4786ac6e6eac
Andres Berejnoi <andresberejnoi@gmail.com>
2021-09-27 16:10:04 -0400

    default parameters were defined
    '

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from organism import Organism
 import matplotlib.pyplot as plt
 
 SIMULATION_PARAMS = {
-    'generations': 1000,
-    'timesteps'  : 100000,   #number of timesteps per generation
+    'generations'   : 1000,
+    'timesteps'     : 100000,   #number of timesteps per generation
+    'mutation_rate' : 0.03,     #not sure if this should go into world parameters or here, or if there should be only one global parameter variable
 }
 
 def main():
