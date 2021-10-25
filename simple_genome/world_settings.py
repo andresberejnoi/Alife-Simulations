906adc5491fd20415b80961c9749ce4f653309e0
Andres Berejnoi <andresberejnoi@gmail.com>
2021-10-25 17:57:19 -0400

    preliminary settings file. It should actually be a text file, but I will keep it like this for now

---
+++ simple_genome/world_settings.py
@@ -0,0 +1,11 @@
+generations           = 10000
+timesteps_per_gen     = 300
+
+initial_population    = 200
+max_population        = None
+
+mutation_rate         = 0.03
+gene_duplication_rate = 0.01
+
+max_hidden_neurons_per_layer = 64
+max_total_neural_connections = 50
