ca107a3ffa92820c6cf89af91efa8955a0e3c4ba
Andres Berejnoi <andresberejnoi@gmail.com>
2021-10-26 19:10:07 -0400

    changed basic settings by name

--- simple_genome/world_settings.py
+++ simple_genome/world_settings.py
@@ -7,5 +7,5 @@ max_population        = None
 mutation_rate         = 0.03
 gene_duplication_rate = 0.01
 
-max_hidden_neurons_per_layer = 64
-max_total_neural_connections = 50
+num_hidden_neurons  = 64
+initial_connections = 50
