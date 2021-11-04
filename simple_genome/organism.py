fb349f7e7712eb6b3f090972695bf1b58a16bc32
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-04 17:20:21 -0400

    Now __repr__ also includes the organism type in the return string

--- simple_genome/organism.py
+++ simple_genome/organism.py
@@ -6,7 +6,7 @@ class Organism(BaseOrganism):
         super().__init__(x_pos=x_pos, y_pos=y_pos, max_x=max_x, max_y=max_y, min_x=min_x, min_y=min_y)
 
     def __repr__(self):
-        return f"<Org: Genome_len={len(self._genome):>3} | NeuralNet Conns={self.nnet.num_connections:>3}, hidden_neurons={self.nnet.num_hidden:>3}>"
+        return f"<Org: Genome_len={len(self._genome):>3} | NeuralNet Conns={self.nnet.num_connections:>3}, hidden_neurons={self.nnet.num_hidden:>3}, type={self.type.upper():>10}>"
 
     def think(self, sensor_funcs={}):
         '''Wrapper for nnet.feedforward to make it easier to access'''
