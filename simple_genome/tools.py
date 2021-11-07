a151bda535a0eb642b6d76f53a2ab50160fd84d2
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-07 13:52:17 -0500

    added function to show information from the organism's internals

--- simple_genome/tools.py
+++ simple_genome/tools.py
@@ -3,13 +3,13 @@ import numpy as np
 from genome import Genome
 
 def get_settings_from_file(filename=''):
-
+    default_file = 'default_config.yml'
     try:
         #filename = "default_config.yml"
         f_handler = open(filename)
-    except FileNotFoundError:
-        f_handler = open('default_config.yml')
-
+    except (FileNotFoundError, TypeError) as e:
+        f_handler = open(default_file)
+        print(f"* Error:\n{e}\n\t* Could not open provided filename: {filename}.\n\t* Opening default file instead: {default_file}")
     config = yaml.safe_load(f_handler)
     f_handler.close()
     return config
@@ -30,4 +30,17 @@ def make_random_genome(num_genes,
     brain_genes[0]  = start_brain_marker
     brain_genes[-1] = end_brain_marker
 
-    return Genome(list(pheno_genes) + list(brain_genes), gene_length=gene_length)
+    return list(pheno_genes) + list(brain_genes)
+
+def show_org_info(org):
+    print(f"\n{'-'*80}")
+    new_line = '\n\t'
+    print(f"\n* Neural net:\n{org.nnet}") 
+    print(f"\n* Neurons:\n\t{new_line.join([str(neuron) for neuron in org.nnet.neurons])}")
+    print(f"\n* Connections:\n\t{new_line.join([str(conn) for conn in org.nnet.connections])}") 
+    print(f"\n* Neuron Accumulator:\n\t{org.nnet.neuron_accumulators}" )
+    #f"\n* Org Genome:\n\t{new_line.join([bin(gene) for gene in org.genome])}" )
+    
+    print("\nGenome:\n")
+    print("\t" + "\n\t".join([f"{gene:032b}" for gene in org.genome]))
+
