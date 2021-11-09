63092f4d470e480362ca93ddf10f01d8ef38b360
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-09 11:08:19 -0500

    completed function to print two orgnisms side by side

--- simple_genome/tools.py
+++ simple_genome/tools.py
@@ -34,13 +34,156 @@ def make_random_genome(num_genes,
 
 def show_org_info(org):
     print(f"\n{'-'*80}")
+    print(f"Organism Details are:\n{org}")
     new_line = '\n\t'
     print(f"\n* Neural net:\n{org.nnet}") 
-    print(f"\n* Neurons:\n\t{new_line.join([str(neuron) for neuron in org.nnet.neurons])}")
-    print(f"\n* Connections:\n\t{new_line.join([str(conn) for conn in org.nnet.connections])}") 
+    print(f"\n* Neurons ({len(org.nnet.neurons)}):\n\t{new_line.join([str(neuron) for neuron in org.nnet.neurons])}")
+    print(f"\n* Connections ({len(org.nnet.connections)}):\n\t{new_line.join([str(conn) for conn in org.nnet.connections])}") 
     print(f"\n* Neuron Accumulator:\n\t{org.nnet.neuron_accumulators}" )
     #f"\n* Org Genome:\n\t{new_line.join([bin(gene) for gene in org.genome])}" )
     
-    print("\nGenome:\n")
+    print("\nGenome (Readable):\n")
     print("\t" + "\n\t".join([f"{gene:032b}" for gene in org.genome]))
 
+    print("\nGenome (list form):")
+    print(f"{org.genome}")
+
+
+
+def show_two_orgs(org1, org2):
+    '''A monster of a function. Provide to Organism objects and it will print their information side by side.
+    This is mostly for debugging. The function looks very ugly as it is, but it works.'''
+    side_len = 80
+    new_line = '\n'
+    sep      = "||"
+    offset   = 2 + len(sep)
+    #---print each org side by side:
+    print(f"{'Org1':^{side_len}} {sep} {'Org2':^{side_len}}")
+    print(f"{'='*((side_len*2) + offset)}")
+    print(f"\nOrganism Details are:\n{str(org1):>{side_len}} {sep} {str(org2):<{side_len}}")
+
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print(f"\nNeural Nets:\n{str(org1.nnet):>{side_len}} {sep} {str(org2.nnet):<{side_len}}")
+
+    #==============================================
+    #---Printing Neurons
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print(f"\n{'Neurons (' + str(len(org1.nnet.neurons)) + ')':^{side_len}} {sep} ", end='')  #using end to connect this print to the next in one line
+    print(f"{'Neurons (' + str(len(org2.nnet.neurons)) + ')' + new_line:^{side_len}}")
+
+
+    #---Printing Neurons
+    neuron_lines = []
+    empty_line = f"{'~'*25:^{side_len}}"
+    for i in range(len(org1.nnet.neurons)):
+        org1_neuron = org1.nnet.neurons[i]
+        line = f"{str(org1_neuron):>{side_len}}"
+        pair = [line, empty_line]
+        neuron_lines.append(pair)
+
+    for i in range(len(org2.nnet.neurons)):
+        org2_neuron = org2.nnet.neurons[i]
+        line = f"{str(org2_neuron):<{side_len}}"
+
+        try:
+            pair = neuron_lines[i]
+            pair[1] = line
+        except IndexError:
+            pair = [empty_line, line]
+            neuron_lines.append(pair)
+
+    list_lines = []
+    for line in neuron_lines:
+        str_line = " || ".join(line)
+        list_lines.append(str_line)
+    
+    str_lines = "\n".join(list_lines)
+    print(f"{str_lines}")
+
+    #==============================================
+    #------------Priting Connections
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print(f"\n{'Connections (' + str(len(org1.nnet.connections)) + ')':^{side_len}} {sep} ", end='')
+    print(f"{'Connections (' + str(len(org2.nnet.connections)) + ')' + new_line:^{side_len}}\n")
+
+
+    conn_lines = []
+    for i in range(len(org1.nnet.connections)):
+        conn = org1.nnet.connections[i]
+        line = f"{str(conn):>{side_len}}"
+        pair = [line, empty_line]
+        conn_lines.append(pair)
+
+    for i in range(len(org2.nnet.connections)):
+        conn = org2.nnet.connections[i]
+        line = f"{str(conn):<{side_len}}"
+
+        try:
+            pair = conn_lines[i]
+            pair[1] = line
+        except IndexError:
+            pair = [empty_line, line]
+            conn_lines.append(pair)
+    
+    list_lines = []
+    for line in conn_lines:
+        str_line = " || ".join(line)
+        list_lines.append(str_line)
+    
+    str_lines = "\n".join(list_lines)
+    print(f"{str_lines}")
+
+
+
+    #=========================================
+    #----------------Printing Neuron Accumulators
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print(f"\n{'Neuron Accumulators (' + str(len(org1.nnet.neuron_accumulators)) + ')':^{side_len}} {sep} ", end='')
+    print(f"{'Neuron Accumulators (' + str(len(org2.nnet.neuron_accumulators)) + ')' + new_line:^{side_len}}\n")
+
+    print(f"{str(org1.nnet.neuron_accumulators):>{side_len}} {sep} {str(org2.nnet.neuron_accumulators):<{side_len}}")
+
+    #==========================================
+    #---------------Printing Genome (readable)
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print(f"\n{'Genome (readable) (' + str(len(org1.genome)) + ')':^{side_len}} {sep} ", end='')
+    print(f"{'Genome (readable) (' + str(len(org2.genome)) + ')' + new_line:^{side_len}}\n")
+
+    genome_lines = []
+    for i in range(len(org1.genome)):
+        gene = org1.genome[i]
+        _line = f"{gene:>032b}"
+        line = f"{_line:>{side_len}}"
+        pair = [line, empty_line]
+        genome_lines.append(pair)
+
+    for i in range(len(org2.genome)):
+        gene = org2.genome[i]
+        _line = f"{gene:<032b}"
+        line = f"{_line:<{side_len}}"
+
+        try:
+            pair = genome_lines[i]
+            pair[1] = line
+        except IndexError:
+            pair = [empty_line, line]
+            genome_lines.append(pair)
+
+    list_lines = []
+    for line in genome_lines:
+        str_line = " || ".join(line)
+        list_lines.append(str_line)
+    
+    str_lines = "\n".join(list_lines)
+    print(f"{str_lines}")
+
+
+    #================================================================
+    #--------------------Printing genome in list form
+    print(f"\n{'-'*((side_len*2) + offset)}")
+    print("\nGenomes in Printable Format:")
+    print(f"\n{'Genome Org1 (list form) (' + str(len(org1.genome)) + '):'}")
+    print(f"\n{org1.genome}")
+
+    print(f"\n{'Genome Org2 (list form) (' + str(len(org2.genome)) + '):'}")
+    print(f"\n{org2.genome}")
