580d1ffc2bc2ce38ceb869cb21516089582f24a8
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-12 02:13:57 -0500

    simplified and cleaned up the code a little bit

--- simple_genome/input_sensors.py
+++ simple_genome/input_sensors.py
@@ -1,64 +1,51 @@
 """Define each sensor with a function"""
 
-def get_sensor(sensor_id):
-    if sensor_id==0:
-        return get_current_x
-    elif sensor_id==1:
-        return get_current_y
-    elif sensor_id==2:
-        return get_last_x
-    elif sensor_id==3:
-        return get_last_y
-    elif sensor_id==4:
-        return get_color_r
-    elif sensor_id==5:
-        return get_color_g
-    elif sensor_id==6:
-        return get_color_b
-    elif sensor_id==7:
-        return get_color_alpha
 
-    #--------Neighbor related sensors
-    elif sensor_id==8:
-        return get_genetic_distance
-    elif sensor_id==9:
-        return get_neihgbor_distance
+def get_sensor(sensor_id):
+    '''Get sensor reading'''
+    return SENSOR_ARRAY[sensor_id]   #people could index SENSOR_ARRAY directly instead of using a function call, but I think this interface is clearer and gives me some flexibility for the future
     
+def get_num_senses():
+    return len(SENSOR_ARRAY)
+
+def read_sensor(org, input_id, **sim_params):
+    sensor = get_sensor(input_id)
+    val = sensor(org, **sim_params)
 
 #-------------LOCATION SENSORS---------------#
-def get_current_x(org):
+def get_current_x(org, **sim_params):
     return org.x_pos
 
-def get_current_y(org):
+def get_current_y(org, **sim_params):
     return org.y_pos
 
-def get_last_x(org):
+def get_last_x(org, **sim_params):
     return org.last_x
 
-def get_last_y(org):
+def get_last_y(org, **sim_params):
     return org.last_y
 
 #-------------COLOR AND APPEARENCES
-def get_color_r(org):
-    return org.color_r
+def get_color_r(org, **sim_params):
+    return org.skin_color_r
 
-def get_color_g(org):
-    return org.color_g
+def get_color_g(org, **sim_params):
+    return org.skin_color_g
 
-def get_color_b(org):
-    return org.color_b
+def get_color_b(org, **sim_params):
+    return org.skin_color_b
 
-def get_color_alpha(org):
-    return org.color_alpha
+def get_color_alpha(org, **sim_params):
+    return org.skin_alpha
 
 #-------------SURROUNDINGS
-def get_biomass_dir_x(org):
+def get_biomass_dir_x(org, **sim_params):
     pass 
 
-def get_biomass_dir_y(org):
+def get_biomass_dir_y(org, **sim_params):
     pass 
 
-def get_biomass_density(org):
+def get_biomass_density(org, **sim_params):
     pass 
 
 
@@ -67,32 +54,77 @@ def get_biomass_density(org):
 # to give a vector <x,y> where the distance represents a relative 
 # magnitude towards the closest neighbor
 #        
-def get_genetic_distance(org1, org2):
-    pass 
+def get_genetic_distance(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
 
-def get_neighbor_dir_x(org):
-    pass 
+def get_neighbor_direction(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return org.direction
 
-def get_neighbor_dir_y(org):
-    pass 
+def get_neighbor_dir_x(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neihgbor_distance(org):
-    pass 
+def get_neighbor_dir_y(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neighbor_age(org):
-    pass 
+def get_neihgbor_distance(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neighbor_color_r(org):
-    pass 
+def get_neighbor_age(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neighbor_color_g(org):
-    pass 
+def get_neighbor_color_r(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neighbor_color_b(org):
-    pass 
+def get_neighbor_color_g(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
-def get_neihgbor_defense(org):
-    pass 
+def get_neighbor_color_b(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
+
+def get_neihgbor_defense(org, **sim_params):
+    closest_org = sim_params.get('closest_org')
+    if org == closest_org:
+        return 0
+     
 
 
-
+#-------Convenient array of sensor functions
+SENSOR_ARRAY = [
+    get_current_x,
+    get_current_y,
+    get_last_x,
+    get_last_y,
+    get_color_r,
+    get_color_g,
+    get_color_b,
+    get_color_alpha,
+    get_genetic_distance,
+    get_neihgbor_distance,
+    get_neighbor_dir_x,
+    get_neighbor_dir_y
+]
