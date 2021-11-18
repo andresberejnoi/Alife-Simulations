3e2f279c2cf8330f27e78848ee53114bd09df2e4
Andres Berejnoi <andresberejnoi@gmail.com>
2021-11-18 13:34:20 -0500

    added method to update organism to new location given motion strength and angle change

--- simple_genome/organism.py
+++ simple_genome/organism.py
@@ -1,5 +1,5 @@
 from basic_types import BaseOrganism
-
+import numpy as np
 
 class Organism(BaseOrganism):
     def __init__(self, x_pos=0, y_pos=0, max_x=100, max_y=100, min_x=0, min_y=0):
@@ -24,5 +24,38 @@ class Organism(BaseOrganism):
         self.y_pos += dy
 
         return self.y_pos, self.x_pos
+
+    def move_forward(self, movement_strength, delta_angle):
+        '''This function needs to be completed. Right now it makes the 
+        assumption that motion is always 1 square away'''
+        possible_moves = {
+            0: (1,0),
+            1: (1,1),
+            2: (0,1),
+            3: (-1,1),
+            4: (-1,0),
+            5: (-1,-1),
+            6: (0,-1),
+            7: (1,-1),
+        }
+        #cur_pos = self.get_pos()
+        #speed   = getattr(self, 'max_speed', 1)  #call the max speed attribute (it should exist in decoder). It is the number of cells the org can move in a single motion. Defaults to 1 if max_speed is not coded for in genome
+
+        #motion_distance = speed * movement_strength  #the actual distance to move
+        
+        new_direction = self.direction + delta_angle
+        
+        if movement_strength==0: #org does not move, but can change angle
+            self.direction = new_direction 
+        
+        else:
+            base_pi = np.pi/4    #pi/4 will result in 8 sections of the circle around current position. It is good for when motion is only 1 square away
+
+            discrete_square = int(new_direction / base_pi) % 8    #will result in a discrete square corresponding to an adjacent square to current position. % 8 assures we only get a value in range [0-7]
+            dx, dy = possible_moves[discrete_square]
+
+            self.update_pos(dx, dy)
+             
+
 
     
